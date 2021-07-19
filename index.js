467e5667a0c64f264c3e0ce15e26d445e1282dbb
ambadi797 <81955216+ambadi797@users.noreply.github.com>
2021-07-19 14:14:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const discord = require('discord.js');
 const { MessageEmbed } = require("discord.js");
 const client = new discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]})
+const client = new Client({ partials: ['MESSAGE'] });
 const prefix = '.';
 
 
